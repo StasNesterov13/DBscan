ba85760db9c713cc945f95905d9726f5fc53411e
Стас <stasnesterov13@gmail.com>
2022-07-17 18:30:21 +0300

    Настройка интерфейса

--- Widget.py
+++ Widget.py
@@ -73,15 +73,14 @@ class Widget(Tk):
 
         self.filemenu = Menu(self.mainmenu, tearoff=0)
         self.filemenu.add_command(label="Загрузить или открыть...", command=self.download_file)
-        self.filemenu.add_command(label="Сохранить...")
+        self.filemenu.add_command(label="Сохранить...", command=self.Writer)
 
         self.methodmenu = Menu(self.mainmenu, tearoff=0)
         self.methodmenu.add_command(label="DBscan", command=self.method_dbscan)
 
         self.boreholemenu = Menu(self.mainmenu, tearoff=0)
-        self.boreholemenu.add_command(label="Отфильтровать...")
-        self.boreholemenu.add_command(label="Отменить...")
-
+        self.boreholemenu.add_command(label="Отфильтровать...", command=self.Remove)
+        self.boreholemenu.add_command(label="Отменить...", command=self.Cancel)
         self.mainmenu.add_cascade(label="Файл",
                                   menu=self.filemenu)
         self.mainmenu.add_cascade(label="Метод",
@@ -99,27 +98,56 @@ class Widget(Tk):
         self.graph_pressure = IntVar()
         self.graph_water_cut = IntVar()
 
+        self.method = 0
+
         self.lbl1 = Label(self, text='Скважина', font="Arial 10", bg='lightblue')
-        self.lbl1.pack(fill='both', side='bottom')
+        self.lbl1.pack(fill='x', side='bottom')
 
         self.lbl2 = Label(self, text='Метод', font="Arial 10", bg='lightblue')
-        self.lbl2.pack(fill='both', side='top')
+        self.lbl2.pack(fill='x', side='top')
 
         self.frame1 = Frame(self)
         self.frame1.config(bg='white')
-        self.frame1.pack(side='top')
+        self.frame1.pack(side='top', fill='x')
 
         self.frame2 = Frame(self)
-        self.frame2.config(bg='white')
-        self.frame2.pack(side='top')
+        self.frame2.config(bg='lightblue')
+        self.frame2.pack(side='right', fill='y', pady=200)
 
         self.frame3 = Frame(self)
         self.frame3.config(bg='white')
-        self.frame3.pack(side='right')
+        self.frame3.pack(side='bottom', fill='both')
 
-        self.frame4 = Frame(self)
-        self.frame3.config(bg='white')
-        self.frame4.pack(side='bottom')
+        check_btn1 = Checkbutton(self.frame2, text="График дебита нефти", variable=self.graph_oil, padx=5, pady=5,
+                                 bg='lightblue')
+        check_btn1.grid(column=0, row=0, sticky='w')
+
+        check_btn2 = Checkbutton(self.frame2, text="График дебита газа", variable=self.graph_gas, padx=5, pady=5,
+                                 bg='lightblue')
+        check_btn2.grid(column=0, row=1, sticky='w')
+
+        check_btn3 = Checkbutton(self.frame2, text="График дебита жидкости", variable=self.graph_water, padx=5, pady=5,
+                                 bg='lightblue')
+        check_btn3.grid(column=0, row=2, sticky='w')
+
+        check_btn4 = Checkbutton(self.frame2, text="График ГФ", variable=self.graph_gf, padx=5, pady=5, bg='lightblue')
+        check_btn4.grid(column=0, row=3, sticky='w')
+
+        check_btn5 = Checkbutton(self.frame2, text="График давления", variable=self.graph_pressure,
+                                 padx=5, pady=5, bg='lightblue')
+        check_btn5.grid(column=0, row=4, sticky='w')
+
+        check_btn6 = Checkbutton(self.frame2, text="График обводненности", variable=self.graph_water_cut,
+                                 padx=5, pady=5, bg='lightblue')
+        check_btn6.grid(column=0, row=5, sticky='w')
+
+        btn4 = Button(self.frame2, text="Построить график", command=self.Graphic, font="Arial 10", bg='white',
+                      width=15)
+        btn4.grid(column=0, row=6, padx=5, pady=5)
+
+        btn5 = Button(self.frame2, text="Сравнить графики", command=self.Graphics, font="Arial 10", bg='white',
+                      width=15)
+        btn5.grid(column=0, row=7, padx=5, pady=5)
 
     def download_file(self):
         download = Download(self)
@@ -127,89 +155,52 @@ class Widget(Tk):
 
     def method_dbscan(self):
         self.lbl2.config(text=f'DBscan')
+        self.method = 1
 
         self.frame1.destroy()
-        self.frame2.destroy()
-        self.frame3.destroy()
 
         self.frame1 = Frame(self)
-        self.frame1.config(bg='white')
-        self.frame1.pack(side='top', fill='x', padx=550)
+        self.frame1.config(bg='lightblue')
+        self.frame1.pack(side='top', padx=200)
 
-        self.frame2 = Frame(self)
-        self.frame2.config(bg='white')
-        self.frame2.pack(side='top', fill='x', padx=575)
+        btn2 = Button(self.frame1, text="K-distances graph", font="Arial 10",
+                      command=lambda: self.Epsilon(int(txt2.get())), bg='white', width=15)
+        btn2.pack(side='right', padx=5, pady=5)
 
-        self.frame3 = Frame(self)
-        self.frame3.config(bg='white')
-        self.frame3.pack(side='right', fill='y')
+        btn1 = Button(self.frame1, text="DBscan", command=lambda: self.DBscan(float(txt1.get()), int(txt2.get())),
+                      font="Arial 10", bg='white', width=15)
+        btn1.pack(side='right', padx=5, pady=5)
 
-        lbl1 = Label(self.frame1, text='Eps:', font="Arial 10", bg='white')
-        lbl1.pack(side='left', padx=10, pady=10)
+        txt2 = Entry(self.frame1, width=10, font="Arial 10", justify='center', bg='white')
+        txt2.pack(side='right', padx=5, pady=5)
 
-        txt1 = Entry(self.frame1, width=10, font="Arial 10", justify='center', bg='lightblue')
-        txt1.pack(side='left', padx=10, pady=10)
+        lbl2 = Label(self.frame1, text='MinElements:', font="Arial 10", bg='lightblue')
+        lbl2.pack(side='right', padx=5, pady=5)
 
-        lbl2 = Label(self.frame1, text='MinElements:', font="Arial 10", bg='white')
-        lbl2.pack(side='left', padx=10, pady=10)
+        txt1 = Entry(self.frame1, width=10, font="Arial 10", justify='center', bg='white')
+        txt1.pack(side='right', padx=5, pady=5)
 
-        txt2 = Entry(self.frame1, width=10, font="Arial 10", justify='center', bg='lightblue')
-        txt2.pack(side='left', padx=10, pady=10)
+        lbl1 = Label(self.frame1, text='Eps:', font="Arial 10", bg='lightblue')
+        lbl1.pack(side='right', padx=5, pady=5)
 
-        btn1 = Button(self.frame2, text="Clustering", font="Arial 10", bg='lightblue', width=15)
-        btn1.grid(column=0, row=0, padx=5, pady=5)
+    def DBscan(self, eps, elements):
+        self.borehole.df_copy = self.borehole.df.copy()
+        dbscan = DBSCAN(eps=eps, min_samples=elements).fit(self.borehole.x_principal)
+        self.borehole.df['Cluster'] = dbscan.labels_
 
-        btn2 = Button(self.frame2, text="DBscan", font="Arial 10", bg='lightblue', width=15)
-        btn2.grid(column=1, row=0, padx=5, pady=5)
-
-        btn3 = Button(self.frame2, text="K-distances graph", font="Arial 10",
-                      command=lambda: self.Epsilon(int(txt2.get())), bg='lightblue', width=15)
-        btn3.grid(columnspan=2, row=1, padx=5, pady=5)
-
-        check_btn1 = Checkbutton(self.frame3, text="График дебита нефти", variable=self.graph_oil, padx=15,
-                                 pady=10, bg='white')
-        check_btn1.grid(column=0, row=0, sticky='w')
-
-        check_btn2 = Checkbutton(self.frame3, text="График дебита газа", variable=self.graph_gas, padx=15,
-                                 pady=10, bg='white')
-        check_btn2.grid(column=0, row=1, sticky='w')
-
-        check_btn3 = Checkbutton(self.frame3, text="График дебита жидкости", variable=self.graph_water, padx=15,
-                                 pady=10, bg='white')
-        check_btn3.grid(column=0, row=2, sticky='w')
-
-        check_btn4 = Checkbutton(self.frame3, text="График ГФ", variable=self.graph_gf, padx=15,
-                                 pady=10, bg='white')
-        check_btn4.grid(column=0, row=3, sticky='w')
-
-        check_btn5 = Checkbutton(self.frame3, text="График давления", variable=self.graph_pressure,
-                                 padx=15, pady=10, bg='white')
-        check_btn5.grid(column=0, row=4, sticky='w')
-
-        check_btn6 = Checkbutton(self.frame3, text="График обводненности", variable=self.graph_water_cut,
-                                 padx=15, pady=10, bg='white')
-        check_btn6.grid(column=0, row=5, sticky='w')
-
-        btn4 = Button(self.frame3, text="Построить график", command=self.Graphics, font="Arial 10", bg='lightblue',
-                      width=15)
-        btn4.grid(column=0, row=6, padx=5, pady=5)
-
-    def Color(self, rainbow):
-        if rainbow:
-            stake_color = list(palette.CSS4_COLORS.values())[10:]
-        else:
-            stake_color = len(set(self.borehole.df['Cluster'])) * ['black']
+    def Color(self):
+        stake_color = len(set(self.borehole.df['Cluster'])) * ['black']
         stake_cluster = set(self.borehole.df['Cluster'])
         clusterColor = dict(zip(stake_cluster, stake_color))
         clusterColor[-1] = 'red'
         return [clusterColor[label] for label in self.borehole.df['Cluster']]
 
     def Epsilon(self, k):
-        self.frame4.destroy()
+        self.frame3.destroy()
 
-        self.frame4 = Frame(self)
-        self.frame4.config(bg='white')
-        self.frame4.pack(side='bottom', fill='both')
+        self.frame3 = Frame(self)
+        self.frame3.config(bg='white')
+        self.frame3.pack(side='bottom', fill='both')
 
         nbrs = NearestNeighbors(n_neighbors=k).fit(self.borehole.x_principal)
         distances, indices = nbrs.kneighbors(self.borehole.x_principal)
@@ -222,32 +213,83 @@ class Widget(Tk):
         plt.ylabel('Epsilon', fontsize=7)
         plt.grid(True)
 
-        canvas = FigureCanvasTkAgg(fig, master=self.frame4)
-        toolbar = NavigationToolbar2Tk(canvas, self.frame4)
+        canvas = FigureCanvasTkAgg(fig, master=self.frame3)
+        toolbar = NavigationToolbar2Tk(canvas, self.frame3)
+        toolbar.update()
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+
+    def Graphic(self):
+        self.frame3.destroy()
+
+        self.frame3 = Frame(self)
+        self.frame3.config(bg='white')
+        self.frame3.pack(side='bottom', fill='both')
+
+        fig = plt.figure(figsize=(20, 12))
+
+        graph = [self.graph_oil.get(), self.graph_gas.get(), self.graph_water.get(), self.graph_gf.get(),
+                 self.graph_pressure.get(), self.graph_water_cut.get()]
+        parametr = [self.borehole.param[6], self.borehole.param[3], self.borehole.param[2], self.borehole.param[10],
+                    self.borehole.param[self.borehole.pressure], self.borehole.param[4]]
+        if 1 in graph:
+            ax = fig.add_subplot()
+            ax.scatter(self.borehole.df.index.tolist(), self.borehole.df[parametr[graph.index(1)]], s=8,
+                       c=self.Color())
+            ax.set(ylabel=f'{parametr[graph.index(1)]}')
+            ax.grid(True)
+
+        canvas = FigureCanvasTkAgg(fig, master=self.frame3)
+        toolbar = NavigationToolbar2Tk(canvas, self.frame3)
         toolbar.update()
         canvas.draw()
         canvas.get_tk_widget().pack()
 
     def Graphics(self):
-        self.frame4.destroy()
+        self.frame3.destroy()
 
-        self.frame4 = Frame(self)
-        self.frame4.config(bg='white')
-        self.frame4.pack(side='bottom', fill='both')
+        self.frame3 = Frame(self)
+        self.frame3.config(bg='white')
+        self.frame3.pack(side='bottom', fill='both')
+
         fig = plt.figure(figsize=(20, 10))
-        ax1 = fig.add_subplot(111)
-        ax2 = ax1.twinx()
 
-        if self.graph_oil.get():
-            ax1.scatter(self.borehole.df.index.tolist(), self.borehole.df[self.borehole.param[6]], s=15, c='red')
-        if self.graph_gas.get():
-            ax2.scatter(self.borehole.df.index.tolist(), self.borehole.df[self.borehole.param[3]], s=15, c='blue')
+        graph = [self.graph_oil.get(), self.graph_gas.get(), self.graph_water.get(), self.graph_gf.get(),
+                 self.graph_pressure.get(), self.graph_water_cut.get()]
+        parametr = [self.borehole.param[6], self.borehole.param[3], self.borehole.param[2], self.borehole.param[10],
+                    self.borehole.param[self.borehole.pressure], self.borehole.param[4]]
+        if 1 in graph:
+            ax1 = fig.add_subplot(2, 1, 1)
+            ax1.scatter(self.borehole.df.index.tolist(), self.borehole.df[parametr[graph.index(1)]], s=8,
+                        c=self.Color())
+            ax1.set(ylabel=f'{parametr[graph.index(1)]}')
+            ax1.grid(True)
+            graph[graph.index(1)] = 0
+            if 1 in graph:
+                ax2 = fig.add_subplot(2, 1, 2)
+                ax2.scatter(self.borehole.df.index.tolist(), self.borehole.df[parametr[graph.index(1)]], s=8,
+                            c=self.Color())
+                ax2.set(ylabel=f'{parametr[graph.index(1)]}')
+                ax2.grid(True)
 
-        canvas = FigureCanvasTkAgg(fig, master=self.frame4)
-        toolbar = NavigationToolbar2Tk(canvas, self.frame4)
+        canvas = FigureCanvasTkAgg(fig, master=self.frame3)
+        toolbar = NavigationToolbar2Tk(canvas, self.frame3)
         toolbar.update()
         canvas.draw()
         canvas.get_tk_widget().pack()
+
+    def Remove(self):
+        self.borehole.df_copy = self.borehole.df.copy()
+        self.borehole.df = self.borehole.df.loc[self.borehole.df['Cluster'] != -1]
+        self.borehole.Standard()
+
+    def Writer(self):
+        writer = pd.ExcelWriter('Two.xlsx')
+        self.borehole.df.iloc[:, :-1].to_excel(writer)
+        writer.save()
+
+    def Cancel(self):
+        self.borehole.df = self.borehole.df_copy.copy()
 
 
 if __name__ == '__main__':
